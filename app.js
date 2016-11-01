d51ed5572fa0af31d41d9dfce9dd3619f9476edd
devonhackley <devonhackley@gmail.com>
2016-11-01 06:46:29 -0700

    adding more to it

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var imgArray = [];
+var userClicks = 0;
+
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+
+function imgProducts (name) {
+  this.name = name;
+  this.path = 'img/' + name + '.jpg';
+  this.numOfDisplays = 0;
+  this.numOfClicks = 0;
+  imgArray.push(this);
+};
+
+function randomNumGen () {
+ var random = 
+ return random;
+
+}
