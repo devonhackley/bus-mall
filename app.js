77c176b25956cc99a6679d8afb54039728ed8311
devonhackley <devonhackley@gmail.com>
2016-11-02 13:07:48 -0700

    adding more to this

--- app.js
+++ app.js
@@ -72,28 +72,21 @@ function randomImg(event) {
     for (var i = 0; i < imgTags.length; i++) {
       imgTags[i].removeEventListener('click', randomImg);
     }
-
-    var userResults = document.getElementById('user-results');
-
-    var ul = document.createElement('ul');
-    userResults.appendChild(ul);
-
-    for (var i = 0; i < productArray.length; i++){
-      var thisProduct = productArray[i];
-      var li = document.createElement('li');
-      var fillerInfo = '';
-      fillerInfo += thisProduct.name;
-      if (thisProduct.numOfDisplays === 0) {
-        fillerInfo += ' | Click Rate: 0%';
-      } else {
-        fillerInfo += ' | Click Rate: ' + (thisProduct.numOfClicks / thisProduct.numOfDisplays * 100) + '%';
-      }
-      li.innerText = fillerInfo;
-      ul.appendChild(li);
-    }
+    // genResults();
+    showResults();
   }
 };
 
+function genResults () {
+  for (var i = 0; i < productArray.length; i++){
+    var thisProduct = productArray[i];
+    if (thisProduct.numOfDisplays === 0) {
+      thisProduct.clickRate = 0;
+    } else {
+      thisProduct.clickRate = (thisProduct.numOfClicks / thisProduct.numOfDisplays * 100);
+    }
+  }
+};
 randomImg();
 userClicks--;
 
@@ -109,45 +102,35 @@ for (var i = 0; i < imgTags.length; i++){
 //   }
 // }
 
-var clickedResults = [];
-for (var i = 0; i < productArray.length; i++){
-  clickedResults.push(productArray[i].numOfClicks);
-}
+function showResults () {
 
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['bag' , 'banana' , 'bathroom' , 'boots' , 'breakfast' , 'bubblegum' ,'chair' , 'cthulhu' , 'dog duck' , 'pen' , 'pet sweep' , 'scissors' , 'shark' , 'sweep' , 'tauntaun' , 'unicorn' , 'usb' , 'water can' , 'wine glass'],
-    datasets: [{
-      label: 'Results',
-      data: clickedResults,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        }
-      }]
-    }
+  var clickedResults = [];
+  for (var i = 0; i < productArray.length; i++){
+    clickedResults.push(productArray[i].numOfClicks);
   }
-});
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['bag' , 'banana' , 'bathroom' , 'boots' , 'breakfast' , 'bubblegum' ,'chair' , 'cthulhu' , 'dog duck' , 'pen' , 'pet sweep' , 'scissors' , 'shark' , 'sweep' , 'tauntaun' , 'unicorn' , 'usb' , 'water can' , 'wine glass'],
+      datasets: [{
+        label: 'Results',
+        data: clickedResults,
+        backgroundColor: ['#ff33bb'],
+        borderColor: ['#0000FF'
+      ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+}
--- data.js
+++ data.js
@@ -1,5 +1,23 @@
 'use strict';
 
 var products = [
-  'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'
+  'bag.jpg',
+  'banana.jpg',
+  'bathroom.jpg',
+  'boots.jpg',
+  'breakfast.jpg',
+  'bubblegum.jpg',
+  'chair.jpg',
+  'cthulhu.jpg',
+  'dog-duck.jpg',
+  'pen.jpg',
+  'pet-sweep.jpg',
+  'scissors.jpg',
+  'shark.jpg',
+  'sweep.png',
+  'tauntaun.jpg',
+  'unicorn.jpg',
+  'usb.gif',
+  'water-can.jpg',
+  'wine-glass.jpg'
 ];
